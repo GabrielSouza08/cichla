3e11616d2de9227a582d0b15b5e51ba1941ea123
Willian Santos <willian.sousa.santos@accenture.com>
2021-10-19 19:47:17 -0300

    Configuração do cors http - Access-Control-Allow-Origin

--- backend_avaliacao-funcionarios/config/server.js
+++ backend_avaliacao-funcionarios/config/server.js
@@ -1,12 +1,18 @@
 /* importar o módulo do framework express */
 const express = require("express");
-const bodyParser = require("body-parser");
+var cors = require('cors')
+
 /* importar o módulo do consign */
 const consign = require("consign");
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    app.use(cors());
+    next()
+ });
 
 /* configurar o middleware body-parser */
 
