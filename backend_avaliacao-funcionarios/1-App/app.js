fe2b45f704f68942e274d7a25f9f8d14412607a0
Gabriel Souza Calixto <gabriel15806@gmail.com>
2021-10-25 23:03:09 -0300

    Subindo codigo com remoção das constraints e inslusão do hash como id

--- backend_avaliacao-funcionarios/1-App/app.js
+++
@@ -1,7 +0,0 @@
-/* importar as configurações*/
-const build = require("../4-Shared/Configuration");
-
-/* parametrizar a porta de escuta */
-build.listen(3030, () => {
-    console.log("Servidor online");
-});
